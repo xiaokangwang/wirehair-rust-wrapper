9154effabb5d7786ac8fd0bc99d9ff2e265f0554
Shelikhoo <xiaokangwang@outlook.com>
2021-11-21 16:12:00 +0000

    update build script

--- build.rs
+++ build.rs
@@ -1,14 +1,27 @@
 extern crate cc;
 
 fn main() {
-    cc::Build::new()
-        .cpp(true)
+    let mut wirehair_build = cc::Build::new();
+
+    wirehair_build.cpp(true)
         .file("src/wirehair/wirehair.cpp")
         .file("src/wirehair/gf256.cpp")
         .file("src/wirehair/WirehairCodec.cpp")
         .file("src/wirehair/WirehairTools.cpp")
         .include("src/wirehair")
         .flag("-msse4.1")
-        .shared_flag(true)
-        .compile("wirehair");
+        .shared_flag(true);
+
+    match std::env::var("TARGET").unwrap().as_str() {
+        "x86_64-unknown-linux-gnu" => {
+            println!("sse4.1 not disabled")
+        }
+        _ => {
+            println!("sse4.1 disabled");
+
+            wirehair_build.define("GF256_TARGET_MOBILE", None);
+        }
+    }
+
+    wirehair_build.compile("wirehair");
 }
